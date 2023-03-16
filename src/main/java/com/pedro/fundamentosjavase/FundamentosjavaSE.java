b375edafcec33dc189ca19e96d0b6c9663d6fa24
pedrofabian455 <pedrofabianzarate@gmail.com>
2023-03-15 21:36:52 -0300

    Inicial Commit actualizacion main

--- src/main/java/com/pedro/fundamentosjavase/FundamentosjavaSE.java
+++ src/main/java/com/pedro/fundamentosjavase/FundamentosjavaSE.java
@@ -11,6 +11,6 @@ package com.pedro.fundamentosjavase;
 public class FundamentosjavaSE {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Pedro");
     }
 }
